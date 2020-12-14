143bdde0502b81df29af0a1b4b4867feeb829a03
BiomageYT <juansebastiancs@ufps.edu.co>
2020-12-14 18:36:12 -0500

    primer commit

---
+++ app/Tienda.php
@@ -0,0 +1,15 @@
+  
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tienda extends Model
+{
+    protected $table = 'tienda';
+
+    protected $fillable = [
+        'id', 'nombre', 'descripcion', 'telefono', 'email', 'pagina_web','imagen'
+    ];
+}
